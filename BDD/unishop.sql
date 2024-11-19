20d2c8458414ea2b785c3976fe9a8976fcc1b9ea
Sberlaiti <152592917+Sberlaiti@users.noreply.github.com>
2024-11-19 17:13:04 +0100

    Update unishop.sql

--- BDD/unishop.sql
+++ BDD/unishop.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : mar. 22 oct. 2024 à 15:07
--- Version du serveur : 8.3.0
--- Version de PHP : 8.2.18
+-- Host: 127.0.0.1:3306
+-- Generation Time: Nov 19, 2024 at 04:12 PM
+-- Server version: 8.3.0
+-- PHP Version: 8.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,165 +18,156 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `unishop`
+-- Database: `unishop`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `avis`
+-- Table structure for table `avis`
 --
 
 DROP TABLE IF EXISTS `avis`;
 CREATE TABLE IF NOT EXISTS `avis` (
-  `IdAvis` int NOT NULL,
-  `Contenue` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
+  `IdAvis` int NOT NULL AUTO_INCREMENT,
+  `Contenue` varchar(500) NOT NULL,
+  `DateCreation` date NOT NULL,
   `IdUtilisateur` int NOT NULL,
   `IdProduit` int NOT NULL,
-  `DateCreation` date NOT NULL,
   PRIMARY KEY (`IdAvis`),
-  KEY `IdUtilisateur` (`IdUtilisateur`),
-  KEY `IdProduit` (`IdProduit`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `fk_avis_utilisateur` (`IdUtilisateur`),
+  KEY `fk_avis_produit` (`IdProduit`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `categorie`
+-- Table structure for table `categorie`
 --
 
 DROP TABLE IF EXISTS `categorie`;
 CREATE TABLE IF NOT EXISTS `categorie` (
-  `IdCategorie` int NOT NULL,
-  `Nom` varchar(130) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`IdCategorie`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `idCategorie` int NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(130) NOT NULL,
+  `IdImage` int NOT NULL,
+  PRIMARY KEY (`idCategorie`),
+  KEY `fk_categorie_iamge` (`IdImage`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `categorie`
+--
+
+INSERT INTO `categorie` (`idCategorie`, `Nom`, `IdImage`) VALUES
+(1, 'Animalerie', 0),
+(2, 'Beauté et parfum', 0),
+(3, 'Téléphonie', 0),
+(4, 'Accessoires UniShop', 0),
+(5, 'Bricolage', 0),
+(6, 'Auto & moto', 0),
+(7, 'Fournitures de bureau', 0),
+(8, 'DVD & Blu-ray', 0),
+(9, 'Cuisine & Maison', 0),
+(10, 'Bagages et accessoires de voyage', 0);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `commande`
+-- Table structure for table `commande`
 --
 
 DROP TABLE IF EXISTS `commande`;
 CREATE TABLE IF NOT EXISTS `commande` (
-  `IdAchat` int NOT NULL,
-  `IdUtilisateur` int NOT NULL,
-  `Date` date NOT NULL,
-  `IdPromo` int NOT NULL,
-  `Adresse` varchar(130) COLLATE utf8mb4_general_ci NOT NULL,
+  `IdCommande` int NOT NULL AUTO_INCREMENT,
+  `DateAchat` date NOT NULL,
+  `Adresse` varchar(130) NOT NULL,
   `Telephone` int NOT NULL,
-  PRIMARY KEY (`IdAchat`),
-  KEY `IdUtilisateur` (`IdUtilisateur`),
-  KEY `IdPromo` (`IdPromo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `IdUtilisateur` int NOT NULL,
+  `IdPromo` int DEFAULT NULL,
+  `IdProduit` int NOT NULL,
+  PRIMARY KEY (`IdCommande`),
+  KEY `fk_commande_utilisateur` (`IdUtilisateur`),
+  KEY `fk_commande_promo` (`IdPromo`),
+  KEY `fk_produit_commande` (`IdProduit`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `imageproduit`
+-- Table structure for table `image`
 --
 
-DROP TABLE IF EXISTS `imageproduit`;
-CREATE TABLE IF NOT EXISTS `imageproduit` (
-  `IdImage` int NOT NULL,
-  `Nom` int NOT NULL,
+DROP TABLE IF EXISTS `image`;
+CREATE TABLE IF NOT EXISTS `image` (
+  `IdImage` int NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(1000) NOT NULL,
   PRIMARY KEY (`IdImage`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `panier`
+-- Table structure for table `panier`
 --
 
 DROP TABLE IF EXISTS `panier`;
 CREATE TABLE IF NOT EXISTS `panier` (
+  `Quantitee` int NOT NULL,
   `IdProduit` int NOT NULL,
   `IdUtilisateur` int NOT NULL,
-  `Quantitée` int NOT NULL,
-  PRIMARY KEY (`IdUtilisateur`),
-  KEY `IdProduit` (`IdProduit`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `fk_produit_panier` (`IdProduit`),
+  KEY `fk_panier_utilisateur` (`IdUtilisateur`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `produit`
+-- Table structure for table `produit`
 --
 
 DROP TABLE IF EXISTS `produit`;
 CREATE TABLE IF NOT EXISTS `produit` (
-  `IdProduit` int NOT NULL,
-  `Nom` varchar(130) COLLATE utf8mb4_general_ci NOT NULL,
-  `Description` varchar(500) COLLATE utf8mb4_general_ci NOT NULL,
-  `Prix` int NOT NULL,
-  `IdImage` int NOT NULL,
+  `IdProduit` int NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(130) NOT NULL,
+  `Description` varchar(500) NOT NULL,
+  `Prix` double NOT NULL,
   `AppartientVendeur` tinyint(1) NOT NULL,
+  `IdImage` int NOT NULL,
+  `DateCreation` date NOT NULL,
   PRIMARY KEY (`IdProduit`),
-  KEY `IdImage` (`IdImage`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `fk_produit_image` (`IdImage`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `promotion`
+-- Table structure for table `promotion`
 --
 
 DROP TABLE IF EXISTS `promotion`;
 CREATE TABLE IF NOT EXISTS `promotion` (
-  `IdPromo` int NOT NULL,
-  `Code` int NOT NULL,
+  `IdPromo` int NOT NULL AUTO_INCREMENT,
+  `CodePromo` varchar(5) NOT NULL,
   `Coefficient` double NOT NULL,
   PRIMARY KEY (`IdPromo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `utilisateur`
+-- Table structure for table `utilisateur`
 --
 
 DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE IF NOT EXISTS `utilisateur` (
-  `IdUtilisateur` int NOT NULL,
-  `Nom` varchar(130) COLLATE utf8mb4_general_ci NOT NULL,
-  `Prenom` varchar(130) COLLATE utf8mb4_general_ci NOT NULL,
-  `Mail` varchar(400) COLLATE utf8mb4_general_ci NOT NULL,
-  `Password` varchar(23) COLLATE utf8mb4_general_ci NOT NULL,
+  `IdUtilisateur` int NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(130) NOT NULL,
+  `Prenom` varchar(130) NOT NULL,
+  `Mail` varchar(400) NOT NULL,
+  `Password` varchar(23) NOT NULL,
   `EstVendeur` tinyint(1) NOT NULL,
   PRIMARY KEY (`IdUtilisateur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `avis`
---
-ALTER TABLE `avis`
-  ADD CONSTRAINT `avis_ibfk_1` FOREIGN KEY (`IdUtilisateur`) REFERENCES `utilisateur` (`IdUtilisateur`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `avis_ibfk_2` FOREIGN KEY (`IdProduit`) REFERENCES `produit` (`IdProduit`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Contraintes pour la table `commande`
---
-ALTER TABLE `commande`
-  ADD CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`IdUtilisateur`) REFERENCES `utilisateur` (`IdUtilisateur`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `commande_ibfk_2` FOREIGN KEY (`IdPromo`) REFERENCES `promotion` (`IdPromo`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Contraintes pour la table `panier`
---
-ALTER TABLE `panier`
-  ADD CONSTRAINT `panier_ibfk_1` FOREIGN KEY (`IdProduit`) REFERENCES `produit` (`IdProduit`) ON DELETE RESTRICT ON UPDATE RESTRICT;
-
---
--- Contraintes pour la table `produit`
---
-ALTER TABLE `produit`
-  ADD CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`IdImage`) REFERENCES `imageproduit` (`IdImage`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
