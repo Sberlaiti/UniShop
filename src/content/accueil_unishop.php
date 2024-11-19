1372b0bbad1cbeeb5b2f50989087ca3381a01cdc
Saber Laiti <saberlaitibenayyad30@gmail.com>
2024-11-19 23:11:08 +0100

    liaison de la BDD avec la page + CSS amélioré mais pas finie

---
+++ src/content/accueil_unishop.php
@@ -0,0 +1,109 @@
+<?php
+    require_once("../connectionBDD/pdo-conn.php");
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    //Récupération des données de la table article
+    $sql_requete = "SELECT p.IdProduit, p.Nom, u.Nom, p.Prix
+                    FROM produit p
+                    JOIN utilisateur u ON u.IdUtilisateur = p.IdUtilisateur";
+    $stmt = $pdo->query($sql_requete);
+
+    //Récupération des valeurs des catégories dans la BDD
+    $sql = "SELECT  categorie.Nom, image.url
+            FROM categorie
+            JOIN image ON categorie.IdImage = image.IdImage";
+    $result = $pdo->query($sql);
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="../style/style.css">
+        <link rel="icon" href="../../logos/logo-png.png" type="image/icon">
+        <title>UniShop</title>
+    </head>
+
+    <body>
+        <!--section>
+            <select id="categorie">
+                <option value="categorie-0">Toutes les catégories</option>
+            </select>
+
+            <input type="search" name="rechercher" placeholder="Rechercher un produit">
+            <button type="submit">Chercher</button>
+        </section-->
+
+        <section class="affichage_produit">
+            <?php
+                if($stmt->rowCount() > 0){
+                    while($row_count = $stmt->fetchAll()){
+                        echo "<div class='produit'>";
+                            echo "<p>" . htmlspecialchars($row_count['Pseudo']) . "</p>";
+                            echo "<h2>" . htmlspecialchars($row_count['Nom']) . "</h2>";
+                            echo "<p>" . htmlspecialchars($row_count['Prix']) . "</p>";
+                        echo "</div>";
+                    }
+                }
+                else{
+                    echo "<div class='no_produit'>
+                            <p>Aucun produit disponible. Revenez un prochain jour !</p>
+                        </div>";
+                }
+            ?>
+
+            <br>
+        </section>
+
+        <section class="affichage_categorie">
+            <div class="title_categories">
+                <h2>Explorez par catégories</h2>
+            </div>
+
+            <?php
+                //Affichage de chaque catégorie
+                if($result->rowCount() > 0){
+                    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+                        echo "<div class='categorie'>";
+                            echo "<img class='image_categorie' src='" . $row['url'] . "' alt='Image de la catégorie'/>";
+                            echo "<p class='categories'>" . htmlspecialchars($row['Nom']) . "</p>";
+                        echo "</div>";
+                    }
+                }
+                else{
+                    echo "<p class='no_categories'>Aucune catégorie disponible</p>";
+                }
+            ?>
+
+        </section>
+
+        <div class="jeu_promo">
+            <p>Jouer au jeu !</p>
+        </div>
+
+        <footer>
+            <div class="return_top">
+                <p id="retourHaut">Retour en haut</p>
+                <script src="../script/fonction.js"></script>
+            </div>
+
+            <div class="logo_langue">
+                <a href="accueil_unishop.php"><img src="../../logos/logo-png.png" width="80" height="50" alt="Logo du site"></a>
+                <select>
+                    <option>Français</option>
+                </select>
+            </div>
+
+            <div class="droits">
+                <div id="liste_droits">
+                    <a href="conditions.php">Conditions générales du site</a>
+                    <a href="">Vos informations personnelles</a>
+                </div>
+                <span>© 2024, UniShop</span>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ src/content/conditions.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../style/conditions.css">
+        <link rel="icon" href="../../logos/logo-png.png" type="image/icon">
+        <title>Coditions générales d'utilisation et de vente - Service client UniShop</title>
+    </head>
+
+    <body>
+        <section class="affichage">
+            <h1>Conditions générales d'utilisation et de vente</h1>
+            <p>Dernière mise à jour le ...</p>
+
+            <p>Bienvenue sur UniShop</p>
+            <p>UniShop est un site de vente en ligne qui propose une gamme variée de produits disponibles à la vente en ligne. 
+                Chaque produit est décrit de manière détaillée sur sa fiche produit. 
+                Les photographies sont les plus fidèles possible mais ne peuvent garantir une parfaite ressemblance avec le produit final.
+                Les utilisateurs peuvent vendre leurs produits en les mettant sur le site, sous réserve de respecter les conditions définies ci-après.
+            </p>
+
+            <hr>
+
+            <h2>1. Inscriptions et accès</h2>
+            <h3>1.1. Création de compte</h3>
+            <p>Pour acheter ou vendre sur UniShop, l'utilisateur doit créer un compte en fournissant des information exactes et complètes. 
+                L'utilisateur est seul responsable de la confidentialité de ses identifiants et de l'utilisation de son compte.
+            </p>
+            
+            <h2>2.2 Accès en tant que vendeur</h2>
+            <p>Pour proposer des produits à la vente, l'utilisateur doit activer le mode vendeur dans son compte et fournir les informations suivantes :</p>
+            <ol>
+                <li>Une </li>
+            </ol>
+            <p>UniShop se réserve le droit de vérifier ces informations et de refuser un accès vendeur en cas de non conformité.</p>
+
+            <h2>3. Prix</h2>
+            <p>Les prix affichés sur le site sont en euros (€) et incluant toutes taxes (TTC). 
+                Les frais de livraisons sont précisées lors de la validation de la commande. 
+                UniShop se réserve le droit de modifier ses prix à tout moment, mais les produits seront facturés sur la base des tarifs en vigueur au moemnt de la validation.
+            </p>
+            
+            <h2>4. Mise en vente des produits</h2>
+            <h3>4.1 Conditions d'éligibilité des produits</h3>
+        </section>
+
+        <footer>
+            <div class="return_top">
+                <p id="retourHaut">Retour en haut</p>
+                <script src="fonction.js"></script>
+            </div>
+
+            <div class="logo_langue">
+                <a href="accueil_unishop.php"><img src="../../logos/logo-png.png" width="80" height="50"></a>
+                <select>
+                    <option>Français</option>
+                </select>
+            </div>
+
+            <div class="droits">
+                <div id="liste_droits">
+                    <a href="conditions.php">Conditions générales du site</a>
+                    <a href="">Vos informations personnelles</a>
+                </div>
+                <span>© 2024, UniShop</span>
+            </div>
+        </footer>
+    </body>
+</html>
