476ba490000154da21c08e25b87c45669a921133
TROTOBAS QUENTIN p2302089 <quentin.trotobas@etu.univ-lyon1.fr>
2024-12-10 14:01:51 +0100

    ajout page liste produit

--- src/js/header.js
+++ src/js/header.js
@@ -4,6 +4,7 @@ const searchButton = searchBar.querySelector('button');
 searchButton.addEventListener('click', display_SearchBar);
 
 function display_SearchBar() {
+    
     let searchInput = searchBar.querySelector('input');
     if (searchInput.style.display === 'block') {
         searchInput.style.display = 'none';
